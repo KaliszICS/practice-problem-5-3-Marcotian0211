42f7049cb4f5a08ff99bc94a2ff3d842466e4c94
Marcotian0211 <350327743@tdsb.ca>
2026-05-11 00:52:07 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,38 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static String[] addString(String[]arr,String word,int num){
+String[]arr2=new String[arr.length+1];
+int index=0;
+for(int i=0;i<arr2.length;i++){
+	if (i!=num){
+		arr2[i]=arr[index];
+		index++;
+	}
+	else{
+		arr2[i]=word;
+	}
+}
+return arr2;
+	}
+public static char[]removeChars(char[]arr,char word){
+	int number=0;
+	int count=0;
+	for(int i=0;i<arr.length;i++){
+		if(arr[i]==word){
+			count++;
+		}
+	}
+	int number2=0;
+	char[]arr3=new char[arr.length-count];
+	for(int q=0;q<arr.length;q++){
+		if(arr[q]!=word){
+			arr3[number2]=arr[q];
+			number2++;
+		}
 
+	}
+	return arr3;
+
+}
 }
